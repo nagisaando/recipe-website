160f6753ed9736756ff9111a150b747225f05603
Nagisa Ando <nagisa@hanayoku.com>
2022-02-05 02:11:07 -0800

    added filter

---
+++ src/components/CuisinesFilter.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="">
+    <button class="flex gap-5 items-center" @click="buttonClickHandler">
+      <h3 class="text-3xl">Cuisines</h3>
+      <i class="fas fa-caret-down | transform" :class="{'rotate-180': openOptions}"></i>
+    </button>
+    <ul v-if="openOptions" class="mt-5">
+      <li v-for="(item, i) in cuisinesOptions" :key="i" class="flex items-center gap-2">
+        <input
+          type="checkbox"
+          :id="item"
+          :value="item"
+          v-model="selectedCuisines"
+          @change="filterChangeHandler"
+        />
+        <label :for="item" class="text-lg">{{ item }}</label>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      openOptions: true,
+      selectedCuisines: [],
+      cuisinesOptions: [
+        'African',
+        'American',
+        'British',
+        'Cajun',
+        'Caribbean',
+        'Chinese',
+        'Eastern European',
+        'European',
+        'French',
+        'German',
+        'Greek',
+        'Indian',
+        'Irish',
+        'Italian',
+        'Japanese',
+        'Jewish',
+        'Korean',
+        'Latin American',
+        'Mediterranean',
+        'Mexican',
+        'Middle Eastern',
+        'Nordic',
+        'Southern',
+        'Spanish',
+        'Thai',
+        'Vietnamese',
+      ],
+    }
+  },
+  methods: {
+    buttonClickHandler() {
+      this.openOptions = !this.openOptions
+    },
+    filterChangeHandler() {
+      this.$store.dispatch('updateSelectedCuisines', this.selectedCuisines)
+      this.$emit('cuisineFilteHandler')
+    },
+  },
+}
+</script>
+
+<style></style>
--- src/store/store.js
+++ src/store/store.js
@@ -4,17 +4,27 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 const state = {
   recipeSearchValue: '',
+  selectedCuisines: [],
 }
-const getters = {}
+const getters = {
+  stringifiedSelectedCuisines(state) {
+    return state.selectedCuisines.join(',')
+  },
+}
 const mutations = {
   UPDATE_RECIPE_SEARCH_VALUE(state, recipeSearchValue) {
     state.recipeSearchValue = recipeSearchValue
-    console.log('mutation done')
+  },
+  UPDATE_SELECTED_CUISINES(state, selectedCuisines) {
+    state.selectedCuisines = selectedCuisines
   },
 }
 const actions = {
   updateRecipeSearchValue({commit}, recipeSearchValue) {
     commit('UPDATE_RECIPE_SEARCH_VALUE', recipeSearchValue)
+  },
+  updateSelectedCuisines({commit}, selectedCuisines) {
+    commit('UPDATE_SELECTED_CUISINES', selectedCuisines)
   },
 }
 export default new Vuex.Store({
