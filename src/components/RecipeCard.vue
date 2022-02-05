6fc3444134c42bf65ed394128c9a93f5d76c5508
Nagisa Ando <nagisa@hanayoku.com>
2022-02-05 02:46:43 -0800

    made ui

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-link :to="`/${recipeInfo.id}`">
+    <img :src="recipeInfo.image" :alt="recipeInfo.title" class="object-cover object-center" />
+    <h1 class="text-xl mt-2">{{ recipeInfo.title }}</h1>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    recipeInfo: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ src/components/RecipeList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="grid gap-y-10 md:gap-5 md:grid-cols-2 lg:grid-cols-3 | mb-20">
+      <Recipe-card v-for="recipe in recipeList" :key="recipe.id" :recipeInfo="recipe" />
+    </div>
+  </div>
+</template>
+
+<script>
+import RecipeCard from './RecipeCard.vue'
+export default {
+  components: {RecipeCard},
+  props: {
+    recipeList: {
+      type: Array,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style></style>
