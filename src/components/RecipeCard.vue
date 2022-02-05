6ba13938cfc5b2e372b22a9fcfbf25eb2607b968
Nagisa Ando <nagisa@hanayoku.com>
2022-02-05 02:54:11 -0800

    added no result found message

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,7 +1,10 @@
 <template>
-  <router-link :to="`/${recipeInfo.id}`">
+  <router-link
+    :to="`/${recipeInfo.id}`"
+    class="hover:opacity-70 transition-all duration-100 ease-in"
+  >
     <img :src="recipeInfo.image" :alt="recipeInfo.title" class="object-cover object-center" />
-    <h1 class="text-xl mt-2">{{ recipeInfo.title }}</h1>
+    <h1 class="text-xl mt-2 font-semibold">{{ recipeInfo.title }}</h1>
   </router-link>
 </template>
 
--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <div class="grid gap-y-10 md:gap-5 md:grid-cols-2 lg:grid-cols-3 | mb-20">
+  <div class="mb-20 | flex-grow">
+    <div class="grid gap-y-10 md:gap-5 md:grid-cols-2 lg:grid-cols-3">
       <Recipe-card v-for="recipe in recipeList" :key="recipe.id" :recipeInfo="recipe" />
     </div>
+    <h2 v-if="recipeList.length === 0" class="text-4xl text-center font-semibold mt-20">
+      No results found
+    </h2>
   </div>
 </template>
 
